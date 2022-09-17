9c7bfd0a29603ee23eed72768656d86b8ef79f19
Samuel Adekunle <oluwatomisinadekunle@gmail.com>
2022-09-17 08:42:27 +0100

    Update credentials

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://aeffec2141d99431a8435b83c763b660-1962042425.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
